a71bedafe2c50865b38974ae7c9759cd97f028f8
marhowlett <marimar_r2003@hotmail.com>
2020-07-14 14:50:53 -0500

    .

--- templates/nosotros.php
+++ templates/nosotros.php
@@ -3,5 +3,152 @@
 get_header();
 ?>
 
+<div class="nostros-row">
+
+<section id="banner-nosotros" class="template-section">
+</section>
+<section id="banner2-nosotros" class="template-section">
+
+ <div class="contenedor">
+        <h2>Pensamos que la vida es <b>demasiado corta</b>
+para tener un <b>negocio que crece lento</b> o
+conformarnos con un mal trabajo que no nos
+permita hacer <b><u>lo que queremos.</u></b></h2>
+        <img class="tarjetas" src="<?php echo get_stylesheet_directory_uri(); ?>/img/nosotros/nosotros.png" alt="Nosotros">
+        <div class="somos">
+            <p class="especial">
+               Somos un grupo de <b>jóvenes creativos</b> con ganas de ayudar a tu negocio a tener la imagen <b>que siempre has soñado.</b>
+            </p>
+            <p class="especial">
+Nuestra meta es que todos vean y aprecien <b>el valor real de lo que vendes.</b>
+            </p>
+        </div>
+        
+        <div class="somos somos2">
+            <p class="especial2">Lo que tenemos claro es que el mercado está cambiando, hoy en día puedes llamar la <b>atención del mundo</b> y de las empresas internacionales <b>sin siquiera salir de casa,</b> no te quedes atrás y aprovecha la magia que el Internet tiene para ti.</p>
+
+<p class="especial2">Estamos tan seguros de que en Internet se pueden hacer negocios, que nuestra agencia es una <b>AGENCIA ONLINE,</b>  con clientes y colaboradores de <b>toda la República.</b></p>
+
+<p><b class="especial">Hoy puedes crecer con más velocidad que nunca.</b></p>
+        </div>
+        
+
+    </div>
+
+
+</section>
+
+<section id="creemos-nosotros" class="template-section">
+
+ <div class="contenedor">
+        <h2>Creemos que <span class="resaltado">cualquier persona</span>  que se atreva a tener un 
+<strong>negocio propio</strong> y a trabajar duro por <span class="resaltado">su marca</span> merece tener
+una imagen donde se respire su <strong>personalidad,</strong> se vea el enorme
+            <strong>potencial</strong> que tienen los productos o servicios que venden y donde 
+un <span class="resaltado">prospecto</span> sea capaz de conectar de una manera 
+     auténtica y de una forma <strong>muy rápida</strong>.</h2>
+        
+
+    </div>
+
+
+</section>
+
+<section id="quienessomos-nosotros" class="template-section">
+<div class="contenedor">
+    <h2>Y a todo esto 
+<strong class="somos">¿Quiénes Somos?</strong></h2>
+<div class="flex">
+    <div class="bloque">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nosotros/karla.jpg" alt="karla">
+    </div>
+     <div class="bloque">
+        <h2 class="nombre"><strong>Karla Hurtado</strong></h2>
+         <p>Hace 7 años estudié la carrera de Diseño Gráfico en la Universidad La Salle Cuernavaca y desde entonces vivo a diariamente el diseño, está en todas partes y estoy segura que más de una vez te ha hecho cambiar la forma de ver las cosas.</p>
+
+<p>Disfruto dibujar, el cine, hacer ejercicio, conocer lugares y gente nueva.</p>
+
+<p>Creo que todos merecemos el éxito, tú mereces ser exitoso, sólo debes ser capaz de confiar en tus deseos, decisiones y tener disciplina. Con esto los puntos se conectaran a tu favor y se hará <b>“la magia”</b> que tanto anhelas.</p>
+         
+     </div>
+</div>
+<div class="flex">
+    
+     <div class="bloque">
+        <h2 class="nombre"><strong>Jesús Velasco</strong></h2>
+         <p>Desde la preparatoria me llamó la atención el Diseño WEB y decidí aprender por mi cuenta, entrando después a la Licenciatura.
+</p>
+<p>En Es Brillante aprendí que hacer un sitio WEB Profesional no sólo es saber programar, sino que también es saber crear sitios <b>que realmente ayuden a nuestros clientes</b> a cerrar ventas por medio de esta herramienta.</p>
+         
+     </div>
+     <div class="bloque">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nosotros/jesus.jpg" alt="karla">
+    </div>
+</div>
+<div class="flex">
+    <div class="bloque">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nosotros/meli.jpg" alt="karla">
+    </div>
+     <div class="bloque">
+        <h2 class="nombre"><strong>Melisa Carvantes</strong></h2>
+         <p>Soy Diseñadora egresada desde el 2011. Me gusta crear, aprender, me encanta escribir y hablar diferentes idiomas.</p>
+
+<p>Creo que el mundo es un lugar fascinante y que las mejores experiencias vienen de tener paciencia y de tener el corazón abierto. Una de mis frases favoritas es <b>“mereces lo que sueñas”</b> porque he tenido la dicha de vivir mis sueños a través de los frutos de mi trabajo y eso le ha dado a esos logros un mejor sabor. Creo que el éxito está al alcance de todos.</p>
+         
+     </div>
+</div>
+</div>
+</section>
+<section id="expertos-nosotros" class="template-section">
+  <div class="contenedor">
+   <h2><strong>15 expertos
+<span>a tu disposición</span></strong></h2>
+   
+   <p>Actualmente somos 15 expertos a tu disposición, entre <b>Copywriters</b> (expertos en redacción persuasiva),<b> diseñadores, programadores, expertos en marketing digital y animación digital</b> (Vídeo animado y edición).</p>
+ 
+<p>En nuestro equipo tenemos la fortuna de trabajar con <b>personas talentosas de diversas partes de México, Costa Rica, Venezuela y Perú.</b> </p>
+ 
+<p>Sabemos que <b>la distancia no es un impedimento</b> y somos conscientes de que Internet puede cambiar la forma en que se hacen los negocios, por lo que no tenemos problemas en contratar personal de diferentes partes del mundo para aportar una perspectiva diferente a cada proyecto con nuestra dirección y la tuya.</p>
+ 
+<p>Además, contamos con una <b>red de colaboradores de áreas complementarias</b> como abogados, ingenieros en sistemas, fotógrafos, ilustradores, traductores y más para que tus necesidades siempre estén cubiertas.</p>
+    </div>
+</section>
+
+         
+         <section id="empresario-nosotros" class="template-section">
+         <div class="contenedor">
+         <div class="flex">
+         <div class="bloque">
+         <h2>Seamos  <span>sinceros</span> <br>
+y hablemos de <span>empresario a</span>
+<strong>empresario</strong></h2>
+      </div>
+      <div class="bloque">
+       <p>
+      Nosotros también somos empresarios, pagamos nómina, tenemos temporadas altas y bajas y hemos invertido con incertidumbre. Sabemos que al comienzo es complicado confiar en un nuevo proveedor, sobre todo si modificará la forma en que se ve tu empresa; puedes estar tranquilo, ya hemos experimentado con más de 250 compañias y hemos aprendido a colaborar con cada uno de ellos para tener buenos resultados.
+      </p>
+             </div>
+        </div>
+         <?php echo do_shortcode('[rev_slider alias="testimonios-nosotros"]'); ?>
+         </div>
+         </section>
+<section id="clientes-nosotros" class="template-section">
+    <div class="contenedor">
+        <h2>¿Quiénes son
+<strong class="somos">nuestros clientes?</strong></h2>
+        <div class="flex nowrap">
+            <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nosotros/roys.jpg" alt="Restaurante Roys Cuernavaca"></a>
+            <a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/nosotros/body.jpg" alt="Body Balance"></a>
+            <a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/inicio/skydive-min.jpg" alt="Skydive México"></a>
+        </div>
+
+        <div class="flex nowrap">
+            <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/inicio/cafealondra-min.jpg" alt="Café Alondra"></a>
+            <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/inicio/donpollo-min.jpg" alt="Don Pollo"></a>
+            <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/inicio/nutricion-min.jpg" alt="Nutrición por tu Salud"></a>
+        </div>
+    </div>
+</section>
+</div>
 
 <?php get_footer(); ?>
